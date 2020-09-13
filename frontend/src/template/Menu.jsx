618e4b78fb13a79428033202c6f3141a91bdf916
rafaelfs96 <rafaelfs96@outlook.com>
2020-09-13 13:37:52 -0300

    change quotes

--- frontend/src/template/Menu.jsx
+++ frontend/src/template/Menu.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default props => (
-  <nav className="navbar navbar-inverse bg-inverse">
-    <div className="container">
-      <div className="navbar-header">
-        <Link to="/" className="navbar-brand">
-          <i className="fa fa-calendar-check-o"> TodoApp</i>
+  <nav className='navbar navbar-inverse bg-inverse'>
+    <div className='container'>
+      <div className='navbar-header'>
+        <Link to='/' className='navbar-brand'>
+          <i className='fa fa-calendar-check-o'> TodoApp</i>
         </Link>
       </div>
-      <div id="navbar" className='navbar-collapse collapse show'>
+      <div id='navbar' className='navbar-collapse collapse show'>
         <ul className='nav navbar-nav'>
-          <li><Link to="/todos">Tarefas</Link></li>
-          <li><Link to="/about">Sobre</Link></li>
+          <li><Link to='/todos'>Tarefas</Link></li>
+          <li><Link to='/about'>Sobre</Link></li>
         </ul>
       </div>
     </div>
